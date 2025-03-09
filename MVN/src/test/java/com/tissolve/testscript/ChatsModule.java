5eb8383531ce37b47be7a523b9245daa426c7ebc
hp <hp@Supriya>
2025-03-09 14:05:13 +0530

    removed the code

--- MVN/src/test/java/com/tissolve/testscript/ChatsModule.java
+++ MVN/src/test/java/com/tissolve/testscript/ChatsModule.java
@@ -14,19 +14,7 @@ import org.testng.annotations.Test;
 public class ChatsModule {
 	@Test
 	public void demo() throws InterruptedException {
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.google.com/");
-		driver.findElement(By.name("q")).sendKeys("Qspiders"+Keys.ENTER);
-		Thread.sleep(2000);
-		List<WebElement> allLinks = driver.findElements(By.xpath("//a"));
-		int count = allLinks.size();
-		System.out.println(count);
-		for(int i=0;i<count;i++) {
-			WebElement ele = allLinks.get(i);
-			String url = ele.getAttribute("href");
-			System.out.println(url);
-		}
-		Reporter.log("Hi",true);
+		
+		Reporter.log("Hello Sridhar",true);
 	}
 }
